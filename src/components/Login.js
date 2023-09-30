1beb82d141f489cda3c3f82d591341af885014ac
Sofia Velasquez <s.velasquezm2@uniandes.edu.co>
2023-09-30 14:34:31 -0500

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
-import { Button, Col, Form, Row, ButtonGroup, Container} from "react-bootstrap";
+import { Button, ButtonGroup, Col, Container, Form, Row } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import "./styles/Login.css";
 
@@ -28,21 +28,35 @@ function Login() {
         // Check if the password is not null (empty or undefined)
         setValidationState((prevState) => ({
             ...prevState,
-            password: password_e !== null && password_e !== undefined && password_e.length > 5 && password_e !== placeholder,
+            password: password_e !== null && password_e !== undefined && password_e.length > 1 && password_e !== placeholder,
         }));
 
         settouched((prevState) => ({ ...prevState, password: true }));
     });
 
     const clickSubmit = () => {
-        // Check if both user and password are not null
-        if (validationState.user && validationState.password ) {
-          // Perform form submission
-          alert(JSON.stringify(formValues));
-          navigate('/cafeList');
-        } else {
-          alert('Correo o contraseña incorrectos');
-        }
+        const URL = 'http://localhost:3001/login';
+        const data = {
+            login: formValues.user,
+            password: formValues.password,
+        };
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        };
+        fetch(URL, requestOptions).then((response) => response.json()).then((data) => {
+            console.log(data);
+            if (data.status === 'success') {
+                alert('Datos correctos');
+                navigate('/cafeList');
+            } else {
+                alert('Datos incorrectos');
+            }
+        }).catch((error) => {
+            console.error('Error fetching data:', error);
+        });
+
     };
 
     const handleCancel = () => {
@@ -51,46 +65,37 @@ function Login() {
 
     return (
         
-        <div className="loginB">
-            <br />
-        <div className="row g-0 justify-content-center  login">
-
-        <div className="col-10 row g-0">
-            <h4 className="loginText"> Inicio de sesión</h4>      
-        </div>
-        <Container className="containerr">
-            <Row className="justify-content-center">
-
+        <Container className="loginB">
             
-        <Form className="col-4 py-4 px-5 mt-5 forms">
-             
-             <br />
-             <Row>
-                  <Form.Label className="formLabel">Nombre de usuario </Form.Label>
-                      <Form.Control className="form-control" type="user"   onChange={handleUserChange} isInvalid={touched.user && !validationState.user}/>
-             </Row>
-                      <br />
-             <Row>
-                          <Form.Label className="formLabel">Contraseña </Form.Label>
-                          <Form.Control className="form-control" type="password"   onChange={handlePasswordChange} isInvalid={touched.password && !validationState.password}/>
-             </Row>
-             <br />
-             <Row>
-                     <Col> 
-                     <ButtonGroup>
-                     <Button className="login-button" size="lg" onClick={clickSubmit} disabled={!(validationState.user && validationState.password)}>Ingresar</Button>{' '}
-                     <Button className="cancel-button" size="lg" onClick={handleCancel}>Cancelar</Button>{' '}
-                     </ButtonGroup>
-                     </Col>
-             </Row>              
-         </Form>  
-         </Row>
- 
-        </Container>
+            <h4 className="loginText"> Inicio de sesión</h4>      
+                <Container className="containerr">
+                    <Row className="justify-content-center">
+
+                    
+                <Form className="py-4 px-5 mt-5 forms">
+                    
+                    <Row>
+                        <Form.Label className="formLabel">Nombre de usuario </Form.Label>
+                            <Form.Control className="form-control" type="text"   onChange={handleUserChange} isInvalid={touched.user && !validationState.user}/>
+                    </Row>
+                    <Row>
+                                <Form.Label className="formLabel">Contraseña </Form.Label>
+                                <Form.Control className="form-control" type="password"   onChange={handlePasswordChange} isInvalid={touched.password && !validationState.password}/>
+                    </Row>
+                    <Row>
+                            <Col> 
+                            <ButtonGroup>
+                            <Button variant="success" size="lg" onClick={clickSubmit}>Ingresar</Button>
+                            <Button variant="danger" size="lg" onClick={handleCancel}>Cancelar</Button>
+                            </ButtonGroup>
+                            </Col>
+                    </Row>              
+                </Form>  
+                </Row>
+                </Container>
 
         
-        </div>
-        </div>
+        </Container>
 
 
     );
